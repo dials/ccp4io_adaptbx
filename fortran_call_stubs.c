f963b588f261ff6259714ed3be0af0b40b482cb4
Ralf W. Grosse-Kuntsleve <rwgkio@gmail.com>
2009-05-31 04:27:53 +0000

    fortran_call_stubs.c: bug fixes, Visual C++ specific prototypes removed for now (for simplicity)

--- fortran_call_stubs.c
+++ fortran_call_stubs.c
@@ -8,31 +8,14 @@ bail(const char* function_name)
   exit(1);
 }
 
-#ifdef _MSC_VER
-#if (CALL_LIKE_MVS==2)
-int IARGC();
-void GETARG(int *i, char *arg, int arg_len);
-void CCP4H_INIT();
-void CCP4H_INIT_LIB(int *ihtml, int *isumm);
-void CCP4H_PRE_BEG();
-void CCP4H_SUMMARY_BEG();
-void CCP4H_SUMMARY_END();
-#else
-int __stdcall IARGC();
-void __stdcall GETARG(int *i, char *arg, int arg_len);
-void __stdcall CCP4H_INIT();
-void __stdcall CCP4H_INIT_LIB(int *ihtml, int *isumm);
-void __stdcall CCP4H_PRE_BEG();
-void __stdcall CCP4H_SUMMARY_BEG();
-void __stdcall CCP4H_SUMMARY_END();
-#endif
-#endif
-
-FORTRAN_SUBR ( IARGC, iargc,
-               (),
-               (),
-               ())
-{ bail("FORTRAN iargc"); }
+FORTRAN_FUN ( int, IARGC, iargc,
+              (),
+              (),
+              ())
+{
+  bail("FORTRAN iargc");
+  return 0;
+}
 
 FORTRAN_SUBR ( GETARG, getarg,
                (int *i, char *arg, int arg_len),
@@ -46,7 +29,7 @@ FORTRAN_SUBR ( CCP4H_INIT, ccp4h_init,
                ())
 { bail("ccp4h_init"); }
 
-FORTRAN_CALL ( CCP4H_INIT_LIB, ccp4h_init_lib,
+FORTRAN_SUBR ( CCP4H_INIT_LIB, ccp4h_init_lib,
                (int* ihtml, int* isumm),
                (int* ihtml, int* isumm),
                (int* ihtml, int* isumm))
